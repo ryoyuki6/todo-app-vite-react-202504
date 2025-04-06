667ccd877274ef2f991c5037fea7bee9f0e39047
Ryo Murakami <ryoyuki6@yahoo.co.jp>
2025-04-06 21:00:37 +0900

    p295

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from "react"
+// import { useState } from 'react'
+import axios from "axios"
+
+const todoDataUrl = "http://localhost:3100/todos";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoList, setTodoList] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(todoDataUrl);
+      setTodoList(response.data);
+    };
+    fetchData();
+  }, []);
 
+  console.log("TODOリスト：", todoList);
+  
   return (
     <>
-      <div>
-        <p>これからTODOアプリを実装しますよー7</p>
-      </div>
+      <h1>TODO進捗管理だよ</h1>
+      <textarea />
+      <button>+ TODOを追加します</button>
+
+      <h2>TODOリストだよ</h2>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>
+            {todo.content}({todo.done ? "完了" : "未完了"})
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
