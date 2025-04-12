0fc40b8f0a682db9f7f0ca1a4a242bb7c3c21cbe
Ryo Murakami <ryoyuki6@yahoo.co.jp>
2025-04-13 08:02:57 +0900

    p312

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,4 @@
-import React, { useState, useEffect } from "react"
-// import { useState } from 'react'
-import axios from "axios"
-
-const todoDataUrl = "http://localhost:3100/todos";
+import { useTodo } from "../hooks/useTodo";
 
 const TodoTitle = ({ title, as }) => {
   if (as === "h1") return <h1>{title}</h1>;
@@ -31,16 +27,7 @@ const TodoList = ({ todoList }) => {
 };
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(todoDataUrl);
-      setTodoList(response.data);
-    };
-    fetchData();
-  }, []);
-  console.log("TODOリスト：", todoList);
+  const { todoList } = useTodo();
 
   const inCompletedList = todoList.filter((todo) => {
     return !todo.done;
